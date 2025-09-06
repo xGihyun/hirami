a552558ff1090a96d46725ca98a48432ee233b14
xGihyun <grdnnz.dev@proton.me>
2025-09-06 09:57:34 +0800

    chore: basic HTTP server setup

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,5 @@
 module github.com/xGihyun/hirami
 
 go 1.24.6
+
+require github.com/joho/godotenv v1.5.1
--- server/main.go
+++ server/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	if err := godotenv.Load(); err != nil {
+		panic("Failed to load .env file.")
+	}
+
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", health)
+
+	host, ok := os.LookupEnv("HOST")
+	if !ok {
+		panic("HOST not found.")
+	}
+
+	port, ok := os.LookupEnv("PORT")
+	if !ok {
+		panic("PORT not found.")
+	}
+
+	server := http.Server{
+		Addr:    host + ":" + port,
+		Handler: router,
+	}
+
+	slog.Info(fmt.Sprintf("Starting server on port: %s", port))
+
+	server.ListenAndServe()
+}
+
+func health(w http.ResponseWriter, r *http.Request) {
+	if err := json.NewEncoder(w).Encode("Hello, World!"); err != nil {
+		slog.Error(err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
