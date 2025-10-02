d1f8664c8d2b6df52e0f26784107de8d83d2fce6
xGihyun <grdnnz.dev@proton.me>
2025-10-02 09:12:45 +0800

    feat(auth/client): implement logout

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,15 +1,24 @@
-import { Link } from "@tanstack/react-router";
+import { Link, useNavigate } from "@tanstack/react-router";
 import { Button } from "./ui/button";
 import type { JSX } from "react";
+import { useAuth } from "@/auth";
 
 export function Navbar(): JSX.Element {
+    const auth = useAuth()
+	const navigate = useNavigate();
+
+	async function handleSignOut(): Promise<void> {
+		await auth.signOut();
+		await navigate({ to: "/login" });
+	}
+
 	return (
 		<header className="p-2 flex gap-2 bg-white text-black justify-between">
 			<nav className="flex flex-row">
 				<div className="px-2 font-bold">
 					<Link to="/equipments">Equipments</Link>
 
-					<Button>Logout</Button>
+					<Button onClick={handleSignOut}>Logout</Button>
 				</div>
 			</nav>
 		</header>
