c5852b0f9d7eea3e4337f72a32c9fa78bbb83213
xGihyun <grdnnz.dev@proton.me>
2025-10-02 08:59:20 +0800

    feat(client/equipment): implement equipment registration

---
+++ client/src/routes/_authed/equipments/-components/register-equipment-form.tsx
@@ -0,0 +1,199 @@
+import type { JSX } from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { toast } from "sonner";
+import type { ApiResponse } from "@/lib/api";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { CalendarIcon } from "lucide-react";
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from "@/components/ui/popover";
+import { cn } from "@/lib/utils";
+import { format } from "date-fns";
+import { Calendar } from "@/components/ui/calendar";
+import { equipmentsQuery } from "@/lib/equipment";
+
+const formSchema = z.object({
+	name: z.string().nonempty(),
+	brand: z.string().optional(),
+	model: z.string().optional(),
+	acquisitionDate: z.date(),
+	quantity: z.number().positive(),
+});
+
+async function register(
+	value: z.infer<typeof formSchema>,
+): Promise<ApiResponse> {
+	console.log(value);
+	const response = await fetch(
+		`${import.meta.env.VITE_BACKEND_URL}/equipments`,
+		{
+			method: "POST",
+			body: JSON.stringify(value),
+			headers: { "Content-Type": "application/json" },
+		},
+	);
+
+	const result: ApiResponse = await response.json();
+	if (!response.ok) {
+		throw new Error(result.message);
+	}
+
+	return result;
+}
+
+export function RegisterEquipmentForm(): JSX.Element {
+	const queryClient = useQueryClient();
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			name: "",
+			brand: "",
+			model: "",
+			acquisitionDate: new Date(),
+			quantity: 1,
+		},
+	});
+
+	const mutation = useMutation({
+		mutationFn: register,
+		onMutate: () => {
+			return toast.loading("Registering equipment");
+		},
+		onSuccess: (data, _variables, toastId) => {
+			queryClient.invalidateQueries(equipmentsQuery);
+			toast.success(data.message, { id: toastId });
+		},
+		onError: (error, _variables, toastId) => {
+			toast.error(error.message, { id: toastId });
+		},
+	});
+
+	async function onSubmit(value: z.infer<typeof formSchema>): Promise<void> {
+		mutation.mutate(value);
+	}
+
+	return (
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+				<FormField
+					control={form.control}
+					name="name"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Name</FormLabel>
+							<FormControl>
+								<Input placeholder="Volleyball" {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+
+				<FormField
+					control={form.control}
+					name="brand"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Brand</FormLabel>
+							<FormControl>
+								<Input placeholder="Mikasa" {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+
+				<FormField
+					control={form.control}
+					name="model"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Model</FormLabel>
+							<FormControl>
+								<Input placeholder="V200W" {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+
+				<FormField
+					control={form.control}
+					name="acquisitionDate"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Acquisition Date</FormLabel>
+							<Popover>
+								<PopoverTrigger asChild>
+									<FormControl>
+										<Button
+											variant={"outline"}
+											className={cn(
+												"w-[240px] pl-3 text-left font-normal",
+												!field.value && "text-muted-foreground",
+											)}
+										>
+											{field.value ? (
+												format(field.value, "PPP")
+											) : (
+												<span>Pick a date</span>
+											)}
+											<CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+										</Button>
+									</FormControl>
+								</PopoverTrigger>
+								<PopoverContent className="w-auto p-0" align="start">
+									<Calendar
+										mode="single"
+										selected={field.value}
+										onSelect={field.onChange}
+										disabled={(date) =>
+											date > new Date() || date < new Date("1900-01-01")
+										}
+										captionLayout="dropdown"
+									/>
+								</PopoverContent>
+							</Popover>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+
+				<FormField
+					control={form.control}
+					name="quantity"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Quantity</FormLabel>
+							<FormControl>
+								<Input
+									type="number"
+									placeholder="Enter quantity"
+									{...field}
+									onChange={(e) => field.onChange(e.target.valueAsNumber)}
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+
+				<Button type="submit">Register Equipment</Button>
+			</form>
+		</Form>
+	);
+}
--- client/src/routes/_authed/equipments/index.tsx
+++ client/src/routes/_authed/equipments/index.tsx
@@ -3,14 +3,21 @@ import { useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 import {
 	Card,
-	CardAction,
 	CardContent,
 	CardDescription,
-	CardFooter,
 	CardHeader,
 	CardTitle,
 } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
+import {
+	Dialog,
+	DialogContent,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/dialog";
+import { RegisterEquipmentForm } from "./-components/register-equipment-form";
+import { Button } from "@/components/ui/button";
 
 export const Route = createFileRoute("/_authed/equipments/")({
 	component: RouteComponent,
@@ -29,10 +36,23 @@ function RouteComponent() {
 		<div className="px-4 lg:px-10 py-5">
 			<h1 className="text-4xl font-bold mb-10">Equipments</h1>
 
+			<Dialog>
+				<DialogTrigger asChild>
+					<Button>Register Equipment</Button>
+				</DialogTrigger>
+				<DialogContent>
+					<DialogHeader>
+						<DialogTitle>Register Equipment</DialogTitle>
+						<RegisterEquipmentForm />
+					</DialogHeader>
+				</DialogContent>
+			</Dialog>
+
 			<div className="grid grid-cols-1 lg:grid-cols-4 gap-2">
-				{data.data.map((equipment) => {
+				{data.map((equipment) => {
+					const key = `${equipment.id}-${equipment.status}`;
 					return (
-						<Card key={equipment.id}>
+						<Card key={key}>
 							<CardHeader>
 								<CardTitle>{equipment.name}</CardTitle>
 								{equipment.brand ? (
