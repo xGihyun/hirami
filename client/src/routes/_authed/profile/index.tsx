c09ba27e7aa44dbaf6a490894d14b6ce1ab55766
xGihyun <grdnnz.dev@proton.me>
2025-10-11 10:45:42 +0800

    feat(profile): add logout button

--- client/src/routes/_authed/profile/index.tsx
+++ client/src/routes/_authed/profile/index.tsx
@@ -1,9 +1,29 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { useAuth } from "@/auth";
+import { Button } from "@/components/ui/button";
+import { createFileRoute } from "@tanstack/react-router";
+import { toast } from "sonner";
 
-export const Route = createFileRoute('/_authed/profile/')({
-  component: RouteComponent,
-})
+export const Route = createFileRoute("/_authed/profile/")({
+	component: RouteComponent,
+});
 
 function RouteComponent() {
-  return <div>Hello "/_authed/profile/"!</div>
+	const auth = useAuth();
+	const navigate = Route.useNavigate();
+
+	async function handleLogout(): Promise<void> {
+		await auth.logout();
+		await navigate({ to: "/login" });
+	}
+
+	return (
+		<div>
+			<Button onClick={handleLogout}>Log Out</Button>
+			<Button
+				onClick={() => toast("TEST", { duration: Number.POSITIVE_INFINITY })}
+			>
+				Toast
+			</Button>
+		</div>
+	);
 }
