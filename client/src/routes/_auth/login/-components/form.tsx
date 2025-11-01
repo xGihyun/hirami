bc2cc8c8283308e267aa7fc98a0a497d9e5be223
xGihyun <grdnnz.dev@proton.me>
2025-11-01 11:18:36 +0800

    feat(client/auth): add login error state

--- client/src/routes/_auth/login/-components/form.tsx
+++ client/src/routes/_auth/login/-components/form.tsx
@@ -1,4 +1,4 @@
-import type { JSX } from "react";
+import { useState, type JSX } from "react";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -18,8 +18,13 @@ import { BACKEND_URL, type ApiResponse } from "@/lib/api";
 import type { User } from "@/lib/user";
 import { useMutation } from "@tanstack/react-query";
 import { setCookie } from "@/lib/cookie";
-import { loginIllustration } from "@/lib/assets";
-import { H1, LabelSmall, TitleSmall } from "@/components/typography";
+import { accessDeniedIllustration, loginIllustration } from "@/lib/assets";
+import {
+	H1,
+	LabelLarge,
+	LabelSmall,
+	TitleSmall,
+} from "@/components/typography";
 import { PasswordInput } from "@/components/password-input";
 
 const formSchema = z.object({
@@ -66,6 +71,8 @@ export function LoginForm(): JSX.Element {
 		mode: "onTouched",
 	});
 
+	const [isError, setIsError] = useState(false);
+
 	const mutation = useMutation({
 		mutationFn: login,
 		onMutate: () => {
@@ -74,15 +81,21 @@ export function LoginForm(): JSX.Element {
 		onSuccess: (result, _variables, toastId) => {
 			setCookie("session", result.data.token);
 			toast.success("Login successful.", { id: toastId });
+			setIsError(false);
 			navigate({ to: "/equipments" });
 		},
-		onError: (error, _variables, toastId) => {
-			toast.error(error.message, { id: toastId });
+		onError: (_error, _variables, toastId) => {
+			toast.dismiss(toastId);
+			setIsError(true);
 		},
 	});
 
 	async function onSubmit(value: z.infer<typeof formSchema>): Promise<void> {
 		mutation.mutate(value);
+	}
+
+	if (isError) {
+		return <Failed onSubmit={form.handleSubmit(onSubmit)} />;
 	}
 
 	return (
@@ -156,6 +169,40 @@ export function LoginForm(): JSX.Element {
 					</LabelSmall>
 				</form>
 			</Form>
+		</div>
+	);
+}
+
+type FailedProps = {
+	onSubmit: () => Promise<void>;
+};
+
+function Failed(props: FailedProps): JSX.Element {
+	return (
+		<div className="h-full w-full flex flex-col gap-12">
+			<section className="space-y-3.5 content-center flex flex-col justify-center items-center h-full">
+				<img
+					src={accessDeniedIllustration}
+					alt="Failed illustration"
+					className="w-full max-w-xs mx-auto"
+				/>
+
+				<div className="space-y-1.5">
+					<H1 className="text-center">Login failed. Please try again.</H1>
+					<TitleSmall className="text-center">
+						A temporary issue occured. Please check your network and Try Again
+						in a moment.
+					</TitleSmall>
+				</div>
+			</section>
+
+			<section className="w-full flex flex-col text-center gap-4">
+				<Button onClick={props.onSubmit}>Try Again</Button>
+
+				<Link to="/login">
+					<LabelLarge>or return to Log In page</LabelLarge>
+				</Link>
+			</section>
 		</div>
 	);
 }
