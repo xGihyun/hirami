4df9a2a2c9195a920273677af6f1571cb4e6ad35
xGihyun <grdnnz.dev@proton.me>
2025-11-06 09:29:36 +0800

    feat(client): adjust navbar z index

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -72,7 +72,7 @@ export function Navbar(): JSX.Element {
 	return (
 		<header
 			className={cn(
-				"gap-2 bg-card text-primary fixed bottom-0 left-0 w-full shadow z-50 pb-[env(safe-area-inset-bottom)] rounded-t-2xl pt-2",
+				"gap-2 bg-card text-primary fixed bottom-0 left-0 w-full shadow z-40 pb-[env(safe-area-inset-bottom)] rounded-t-2xl pt-2",
 				location.search.success !== undefined ? "hidden" : "flex",
 			)}
 		>
