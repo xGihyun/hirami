29273754be856c51709d97e8ea4cc6475c71aa72
xGihyun <grdnnz.dev@proton.me>
2025-11-07 08:26:52 +0800

    feat(client/catalog): hide category scrollbar

--- client/src/routes/_authed/equipments/-components/catalog-categories.tsx
+++ client/src/routes/_authed/equipments/-components/catalog-categories.tsx
@@ -98,7 +98,7 @@ export function CatalogCategories(props: Props): JSX.Element {
 					))}
 				</div>
 
-				<ScrollBar orientation="horizontal" />
+				<ScrollBar orientation="horizontal" hidden />
 			</ScrollArea>
 		</section>
 	);
