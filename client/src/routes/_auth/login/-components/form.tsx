18bb4a30024b0057602ccb8ef5b4accdcf495977
xGihyun <grdnnz.dev@proton.me>
2025-11-07 10:28:08 +0800

    feat(client/auth): remove temporary links

--- client/src/routes/_auth/login/-components/form.tsx
+++ client/src/routes/_auth/login/-components/form.tsx
@@ -170,16 +170,6 @@ export function LoginForm(): JSX.Element {
 							Click here
 						</Link>
 					</LabelSmall>
-
-					<LabelSmall className="text-center">
-						No account?{" "}
-						<Link
-							to="/register"
-							className="font-montserrat-bold underline"
-						>
-                            Register
-						</Link>
-					</LabelSmall>
 				</form>
 			</Form>
 		</div>
