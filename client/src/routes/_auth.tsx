5b76099be5cf5adf9e551447cf9a4dc059a73c9b
xGihyun <grdnnz.dev@proton.me>
2025-11-07 09:48:58 +0800

    refactor(client/auth): remove unused imports

--- client/src/routes/_auth.tsx
+++ client/src/routes/_auth.tsx
@@ -1,14 +1,5 @@
-import { hiramiLogoDarkWithTitle } from "@/lib/assets";
-import {
-	createFileRoute,
-	Link,
-	Outlet,
-	redirect,
-	useLocation,
-} from "@tanstack/react-router";
+import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
 import type { JSX } from "react";
-import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Card, CardContent } from "@/components/ui/card";
 
 export const Route = createFileRoute("/_auth")({
 	component: RouteComponent,
@@ -21,15 +12,6 @@ export const Route = createFileRoute("/_auth")({
 });
 
 function RouteComponent(): JSX.Element {
-	const location = useLocation();
-	function getCurrentTab(): "login" | "register" {
-		if (location.pathname.includes("/register")) {
-			return "register";
-		}
-
-		return "login";
-	}
-
 	return (
 		<div className="bg-background flex flex-col h-svh pt-[env(safe-area-inset-top)] pb-[env(safe-area-inset-bottom)] px-4">
 			<Outlet />
