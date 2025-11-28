8e6221f54eb30377c770a7022e9cc6253a70a9c1
xGihyun <grdnnz.dev@proton.me>
2025-11-28 13:02:52 +0800

    feat(client): listen to borrow request review updates on authed layout

--- client/src/routes/_authed.tsx
+++ client/src/routes/_authed.tsx
@@ -1,6 +1,15 @@
 import { Navbar } from "@/components/navbar";
+import { BACKEND_URL } from "@/lib/api";
 import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
-import type { JSX } from "react";
+import { useEffect, type JSX } from "react";
+import { EventSource } from "eventsource";
+import {
+	BorrowRequestStatus,
+	type ReviewBorrowResponse,
+} from "@/lib/equipment/borrow";
+import { toast } from "sonner";
+import { useAuth } from "@/auth";
+import { UserRole } from "@/lib/user";
 
 export const Route = createFileRoute("/_authed")({
 	component: RouteComponent,
@@ -12,6 +21,38 @@ export const Route = createFileRoute("/_authed")({
 });
 
 function RouteComponent(): JSX.Element {
+	const auth = useAuth();
+
+	useEffect(() => {
+		if (auth.user?.role.code === UserRole.EquipmentManager) {
+			return;
+		}
+
+		const eventSource = new EventSource(`${BACKEND_URL}/events`);
+
+		function handleEvent(e: MessageEvent<string>): void {
+			const res = JSON.parse(e.data) as ReviewBorrowResponse;
+			if (res.status.code === BorrowRequestStatus.Approved) {
+				toast.success("Your borrow request has been approved.", {
+					duration: Number.POSITIVE_INFINITY,
+				});
+				return;
+			}
+
+			toast.success("Your borrow request has been rejected.", {
+				duration: Number.POSITIVE_INFINITY,
+			});
+		}
+
+		const listenerType = "borrow-request:review";
+		eventSource.addEventListener(listenerType, handleEvent);
+
+		return () => {
+			eventSource.removeEventListener(listenerType, handleEvent);
+			eventSource.close();
+		};
+	}, []);
+
 	return (
 		<div className="pt-[calc(1.25rem+env(safe-area-inset-top))] pb-[calc(5rem+env(safe-area-inset-bottom))] px-4 lg:px-10">
 			<Outlet />
