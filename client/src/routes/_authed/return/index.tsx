d5820dc21372bb4f5e242fda35dc1aa2142aa7ed
xGihyun <grdnnz.dev@proton.me>
2025-11-30 13:48:20 +0800

    fix(client/return): properly show the success/error states when requests are submitted

--- client/src/routes/_authed/return/index.tsx
+++ client/src/routes/_authed/return/index.tsx
@@ -1,12 +1,9 @@
 import { useAuth } from "@/auth";
 import { BACKEND_URL, Sort } from "@/lib/api";
-import {
-	borrowedItemsQuery,
-} from "@/lib/equipment/borrow";
+import { borrowedItemsQuery } from "@/lib/equipment/borrow";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
-import { createFileRoute, useSearch } from "@tanstack/react-router";
-import { useEffect, type JSX } from "react";
-import { ReturnEquipmentForm } from "./-components/return-equipment-form";
+import { createFileRoute } from "@tanstack/react-router";
+import { useEffect, useState, type JSX } from "react";
 import { returnRequestsQuery } from "@/lib/equipment/return";
 import { equipmentNamesQuery } from "@/lib/equipment";
 import { EventSource } from "eventsource";
@@ -16,6 +13,8 @@ import { ReturnTab } from "./-model";
 import { ReturnRequestList } from "./-components/return-request-list";
 import { ComponentLoading } from "@/components/loading";
 import { LabelMedium } from "@/components/typography";
+import { BorrowedItemList } from "./-components/borrowed-item-list";
+import { Success } from "@/components/success";
 
 const searchSchema = z.object({
 	tab: z.enum(ReturnTab).default(ReturnTab.BorrowedItems),
@@ -41,8 +40,9 @@ export const Route = createFileRoute("/_authed/return/")({
 });
 
 function RouteComponent(): JSX.Element {
-	const search = useSearch({ from: "/_authed/return/" });
+	const search = Route.useSearch();
 	const auth = useAuth();
+	const [isConfirmed, setIsConfirmed] = useState(false);
 
 	// NOTE: Naive way to make sure the available options in equipment names are
 	// only the equipments included in the history or return request
@@ -58,7 +58,7 @@ function RouteComponent(): JSX.Element {
 			sort: search.dueDateSort,
 		}),
 	);
-    // NOTE: Getting the unique names should ideally be done on the server
+	// NOTE: Getting the unique names should ideally be done on the server
 	const historyEquipmentNames = Array.from(
 		new Set(
 			borrowHistoryAllCategory.data?.flatMap((history) =>
@@ -87,32 +87,45 @@ function RouteComponent(): JSX.Element {
 			queryClient.invalidateQueries(
 				borrowedItemsQuery({
 					userId: auth.user?.id,
-					sort: search.dueDateSort,
-					category: search.category,
 				}),
 			);
 			queryClient.invalidateQueries(
 				returnRequestsQuery({
 					userId: auth.user?.id,
-					sort: search.dueDateSort,
 				}),
 			);
+
+			setIsConfirmed(true);
 		}
 
-		eventSource.addEventListener("equipment:create", handleEvent);
+		eventSource.addEventListener("return-request:confirm", handleEvent);
 
 		return () => {
-			eventSource.removeEventListener("equipment:create", handleEvent);
+			eventSource.removeEventListener("return-request:confirm", handleEvent);
 			eventSource.close();
 		};
 	}, []);
+
+	function reset(): void {
+		setIsConfirmed(false);
+	}
+
+	if (isConfirmed) {
+		return (
+			<Success
+				fn={reset}
+				header="Successfully returned equipments."
+				backLink="/return"
+			/>
+		);
+	}
 
 	return (
 		<main className="space-y-4 pb-15 !mb-0">
 			<ReturnHeader equipmentNames={equipmentNames || []} />
 
 			{search.tab === ReturnTab.BorrowedItems ? (
-				<BorrowedItemsTab />
+				<BorrowedItemList />
 			) : (
 				<ReturnRequestListTab />
 			)}
@@ -120,34 +133,8 @@ function RouteComponent(): JSX.Element {
 	);
 }
 
-function BorrowedItemsTab(): JSX.Element {
-	const search = useSearch({ from: "/_authed/return/" });
-	const auth = useAuth();
-	const borrowHistory = useQuery(
-		borrowedItemsQuery({
-			userId: auth.user?.id,
-			sort: search.dueDateSort,
-			category: search.category,
-		}),
-	);
-
-	if (borrowHistory.isLoading) {
-		return <ComponentLoading />;
-	}
-
-	if (borrowHistory.data?.length === 0) {
-		return (
-			<LabelMedium className="text-muted text-center mt-10">
-				No borrowed equipment found
-			</LabelMedium>
-		);
-	}
-
-	return <ReturnEquipmentForm transactions={borrowHistory.data || []} />;
-}
-
 function ReturnRequestListTab(): JSX.Element {
-	const search = useSearch({ from: "/_authed/return/" });
+	const search = Route.useSearch();
 	const auth = useAuth();
 	const returnRequests = useQuery(
 		returnRequestsQuery({
