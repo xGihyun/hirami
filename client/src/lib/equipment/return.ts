69eb6c2cd1cad701618767557cd1802fd9363826
xGihyun <grdnnz.dev@proton.me>
2026-03-06 11:27:44 +0800

    chore: remove unused file

--- client/src/lib/equipment/return.ts
+++
@@ -1,6 +0,0 @@
-import { queryOptions } from "@tanstack/react-query";
-import { BACKEND_URL, Sort, type ApiResponse } from "../api";
-import type { OTP } from "./borrow";
-import type { Equipment } from "./model";
-import type { UserBasicInfo } from "../user/model";
-
