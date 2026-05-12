22c154fddb00fb07dc31195c9a63005c8cc0f304
xGihyun <grdnnz.dev@proton.me>
2026-05-12 13:58:43 +0800

    prioritize redirecting to landing

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,7 +1,11 @@
 import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
-	beforeLoad: () => {
-		throw redirect({ to: "/equipments" });
+	beforeLoad: async ({ context }) => {
+		const session = await context.auth.validateSession();
+		if (session) {
+			throw redirect({ to: "/equipments" });
+		}
+		throw redirect({ to: "/landing" });
 	},
 });
